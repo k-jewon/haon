c2d482d10b27a5c930e5953f3722a8aee0a9a24b
k-jewon <kjw2051182@gmail.com>
2023-03-03 11:11:17 +0900

    userVO update

--- src/main/java/kr/co/haon/user/userVO.java
+++ src/main/java/kr/co/haon/user/userVO.java
@@ -1,5 +1,20 @@
 package kr.co.haon.user;
 
 public class userVO {
-
+	private int User_Id;
+	private String User_Email;
+	public int getUser_Id() {
+		return User_Id;
+	}
+	public void setUser_Id(int user_Id) {
+		User_Id = user_Id;
+	}
+	public String getUser_Email() {
+		return User_Email;
+	}
+	public void setUser_Email(String user_Email) {
+		User_Email = user_Email;
+	}
+	
+	
 }
