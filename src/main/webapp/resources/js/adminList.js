b338dcb001b669bb2de058bf48c7fff62dd5ac92
wndms05 <juen0514@naver.com>
2023-03-09 11:11:19 +0900

    관리자 단 index mapper 작성 및 세부 정리

---
+++ src/main/webapp/resources/js/adminList.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+ 
+function payUpdate() {
+
+	if(confirm("결제를 취소하시겠습니까?")){
+	                alert("결제 취소되었습니다.");
+	            }else{  
+	            }
+}
+
+function paydelete() {
+  	if(confirm("결제 정보를 영구 삭제하시겠습니까?")){
+	       confirm("삭제되었습니다.");
+	}
+	//if else(pay_static==='결제취소') {  
+	//       confirm("결제 취소 후 삭제해 주세요");
+ 	//}
+	 
+}
